10605745616ad079056cf4e5894846ef25c647ce
Patel Hetvi <hetvipatel4728@gmail.com>
2026-09-16 19:18:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Smaller in Array/count-smaller-in-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int countOfElements(int x, vector<int> &arr) {
+        int count = 0;
+        
+        for(int i = 0; i < arr.size(); i++){
+            if(arr[i] <= x){
+                count++;
+            }
+            else{
+                continue;
+            }
+        }
+        return count;
+    }
+};
