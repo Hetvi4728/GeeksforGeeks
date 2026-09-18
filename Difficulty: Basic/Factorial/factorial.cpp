d099a07f0cde3e4bac1fcdcb49ee116f38c66356
Patel Hetvi <hetvipatel4728@gmail.com>
2026-09-18 21:09:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int factorial(int n) {
+        int fact = 1;
+        
+        while(n != 0){
+            fact = fact * n;
+            n = n - 1;
+        }
+        return fact;
+    }
+};
